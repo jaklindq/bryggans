5a3367c4bb8f03e79dca3360297b6351b9de551a
Jakob Lindqvist <jakob.lindqvist@zenuity.com>
2018-05-24 08:17:30 +0200

    Non-important info format

--- Departement/Bryggeri/recipe/setup.py
+++ Departement/Bryggeri/recipe/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='recipe',
     version='0.0.1',
-    description='a pip-installable package for Beer-XML recipes',
+    description='A pip-installable package for Beer-XML recipes',
     license='MIT',
     packages=['recipe'],
     author='Jakob Lindqvist',
